1742f7fd38bf7324cf15d631cb37869d96d546db
hawk7227 <marcus@banktrustdeals.com>
2026-01-26 11:00:53 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Skip TypeScript errors during build
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // Skip ESLint errors during build
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // Other config options
+  images: {
+    domains: ['picsum.photos', 'images.unsplash.com', 'm.media-amazon.com', 'i.imgur.com'],
+  },
+};
 
+module.exports = nextConfig;
